d39b046379dd11958e062903f9507e65188e841d
Jisu Lee <jaejisu@gmail.com>
2018-11-25 12:29:31 -0800

    commit 1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="xxx">Hello CodeSandbox</h1>
+      <h1 className="xxx">Hello CodeSandbox Made By Jisu</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
